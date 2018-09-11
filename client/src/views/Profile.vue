37cba98b59c12ffada39ec45068283e978156fe7
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-09-11 14:42:55 -0600

    push

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Profile">
+
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Profile',
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        components: {}
+    }
+</script>
+
+
+<style scoped>
+
+</style>
