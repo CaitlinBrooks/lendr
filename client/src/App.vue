9bcd0fdb7173b2f88a2ae8b51ac1d67936514e5f
CaitlinCodes <caiit@me.com>
2018-09-13 16:51:00 -0600

    @click for logout

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
       <v-list>
         <v-list-tile value="true" v-for="(item, i) in items" :key="i">
           <v-list-tile-content>
-            <v-list-tile-title item.action v-text="item.title" class="teal--text text--lighten-2"></v-list-tile-title>
+            <v-list-tile-title @click='item.action' v-text="item.title" class="teal--text text--lighten-2"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -53,7 +53,7 @@
         },
         {
           title: 'Logout',
-          // action: "@click='logout'" vbind @click
+          action: this.logout
         }],
         miniVariant: false,
         right: true,
@@ -63,6 +63,19 @@
     computed: {
       currentRoute() {
         return this.$route.name
+      }
+    },
+    methods: {
+      logout() {
+        console.log('Goodbye')
+        // auth.delete('logout')
+        //   .then(res => {
+        //     router.push({ name: 'login' })
+
+        //   })
+      },
+      test() {
+        console.log('what')
       }
     }
   }
