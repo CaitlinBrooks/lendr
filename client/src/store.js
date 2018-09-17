84f9d85a08891155020f8d7e1a33b949665bdac2
CaitlinCodes <caiit@me.com>
2018-09-17 16:00:10 -0600

    ts ignore

--- client/src/store.js
+++ client/src/store.js
@@ -244,6 +244,7 @@ export default new Vuex.Store({
       socket.on('newMessage', data => {
         // @ts-ignore
         console.log("message for" + data)
+        // @ts-ignore
         if (data == this.state.user._id) {
           dispatch('getBorrows', data)
           console.log("You have a new borrow!")
