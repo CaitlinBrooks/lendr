e4cb666e53d6f91268923ec9425ed13e2d3c66c9
Christopher Carlson <c.m.carlson13@gmail.com>
2018-09-18 09:12:19 -0600

    snackbar link works

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -52,7 +52,7 @@
             <v-snackbar v-model="snackbar" :bottom="y === 'bottom'" :left="x === 'left'" :multi-line="mode === 'multi-line'" :right="x === 'right'"
                 :timeout="timeout" :top="y === 'top'" :vertical="mode === 'vertical'">
                 You have unread borrows!
-                <v-btn color="pink" flat @click="snackbar = false">
+                <v-btn color="pink" flat @click="viewLends">
                     View
                 </v-btn>
             </v-snackbar>
