4defa7ceae4654316bdfd21862712f3176ca51d3
CaitlinCodes <caiit@me.com>
2018-09-19 11:48:13 -0600

    nodemailer revisions

--- server/nodemailer/nodemailer.js
+++ server/nodemailer/nodemailer.js
@@ -1,5 +1,5 @@
 let router = require('express').Router()
-let nodemailer = require('nodemailer'); //const?
+let nodemailer = require('nodemailer');
 
 
 nodemailer.createTestAccount((err, account) => {
@@ -10,13 +10,16 @@ nodemailer.createTestAccount((err, account) => {
     auth: {
       user: account.user,
       pass: account.pass
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
   let mailOptions = {
     from: '"Lendr" <lendr@lends.com>',
     to: 'you@example.com',
     subject: 'Hello! Your lend is expiring soon.',
-    text: 'Please check your lend history to return your item on time.',
+    text: 'Please check your lend history to return your item on time.', //output
     // html: 'html body'
   };
   transporter.sendMail(mailOptions, (error, info) => {
@@ -25,6 +28,8 @@ nodemailer.createTestAccount((err, account) => {
     }
     console.log('Message sent: %s', info.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    // res.render('message', { msg: 'Email has been sent' })
   });
 });
 
