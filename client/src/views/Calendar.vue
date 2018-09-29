d4d7bc8a7109e4277645eed50f6e754e2c90dd2e
Christopher Carlson <c.m.carlson13@gmail.com>
2018-09-28 23:43:39 -0600

    Calendar fixes

--- client/src/views/Calendar.vue
+++ client/src/views/Calendar.vue
@@ -1,8 +1,22 @@
 <template>
   <!-- CALENDAR -->
-  <v-flex xs12 sm6 class="my-3 mx-3">
-    <v-date-picker v-model="myCal" :events="dueDates" :event-color="date => checkDate(date)" color="green lighten-1" header-color="teal darken-2"></v-date-picker>
-  </v-flex>
+  <v-container fluid fill-height>
+    <v-layout row wrap justify-center full-height align-center>
+      <v-flex xs12 sm6 class="my-3 mx-3">
+        <v-flex text-xs-center>
+          <p class="display-2 font-weight-medium">
+            My Calendar
+          </p>
+        </v-flex>
+        <v-layout>
+          <v-flex>
+            <v-date-picker full-width landscape v-model="myCal" :events="dueDates" :event-color="date => checkDate(date)"
+              color="green lighten-1" header-color="teal darken-2"></v-date-picker>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
