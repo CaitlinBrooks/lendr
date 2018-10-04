08be4771792939ba8bc44ea7ed3e8560b1caf47e
Christopher Carlson <c.m.carlson13@gmail.com>
2018-10-04 16:20:53 -0600

    fixed calendars

--- client/src/views/Calendar.vue
+++ client/src/views/Calendar.vue
@@ -9,9 +9,13 @@
           </p>
         </v-flex>
         <v-layout>
-          <v-flex>
+          <v-flex class="desktop-calendar">
             <v-date-picker full-width landscape v-model="myCal" :events="dueDates" :event-color="date => checkDate(date)"
               color="green lighten-1" header-color="teal darken-2"></v-date-picker>
+          </v-flex>
+          <v-flex class="mobile-calendar">
+            <v-date-picker v-model="myCal" :events="dueDates" :event-color="date => checkDate(date)" color="green lighten-1"
+              header-color="teal darken-2"></v-date-picker>
           </v-flex>
         </v-layout>
       </v-flex>
@@ -48,3 +52,32 @@
     }
   }
 </script>
+
+<style>
+  .mobile-calendar {
+    display: none;
+  }
+
+  .desktop-calendar {
+    display: unset;
+  }
+
+  @media only screen and (max-width : 450px) {
+    .mobile-calendar {
+      display: flex;
+      justify-content: center;
+    }
+
+    .child-flex>*,
+    .flex {
+      -webkit-box-flex: 1;
+      -ms-flex: 1 1 auto;
+      flex: auto;
+      max-width: 100%;
+    }
+
+    .desktop-calendar {
+      display: none;
+    }
+  }
+</style>
