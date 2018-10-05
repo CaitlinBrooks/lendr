30a71d979cc259cf9797ccefe311080be9cdd394
Christopher Carlson <c.m.carlson13@gmail.com>
2018-10-05 10:36:37 -0600

    fixed lends page

--- client/src/views/Lends.vue
+++ client/src/views/Lends.vue
@@ -3,22 +3,24 @@
     <v-layout row wrap justify-space-between>
       <!-- Lends -->
       <v-flex xs12 md4 two-line>
-        <div class="display-3">
+        <div class="display-1">
           My Lends
         </div>
         <v-divider></v-divider>
         <v-expansion-panel>
           <v-expansion-panel-content color="teal" v-for="(lend, i) in this.myLends" :key="i">
             <div slot="header">{{lend.title}}</div>
-            <v-card>
-              <v-list-tile-sub-title>{{lend.description}}</v-list-tile-sub-title>
-              <v-layout row>
-                <div v-if=lend.borrower.returned>
+            <v-card color="grey darken-3 white--text">
+              <div class="pl-4">{{lend.description}}</div>
+              <v-layout row class="pt-2">
+                <div v-if=lend.borrower.returned class="pl-4 pt-2">
                   This item has been returned
                 </div>
-                <div v-else>
+                <div v-else class="pl-3">
                   <v-btn color="teal" @click="validateReturn(lend)">Has this been returned?</v-btn>
                 </div>
+              </v-layout>
+              <v-layout row class="pt-2 pl-4">
                 <div v-if=lend.lendr.lent>
                   Your borrower has recieved this.
                 </div>
@@ -32,30 +34,32 @@
       </v-flex>
       <!-- Borrows -->
       <v-flex xs12 md4 two-line>
-        <div class="display-3">
+        <div class="display-1">
           My Borrows
         </div>
         <v-divider></v-divider>
         <v-expansion-panel>
           <v-expansion-panel-content color="teal" v-for="(borrow, i) in this.myBorrows" :key="i">
             <div slot="header">{{borrow.title}}</div>
-            <v-container justify-center>
-              <v-card>
-                <v-list-tile-sub-title justify-center>{{borrow.description}}</v-list-tile-sub-title>
-                <div v-if=borrow.lendr.lent justify-center>
+            <v-card color="grey darken-3 white--text">
+              <div class="pl-4">{{borrow.description}}</div>
+              <v-layout row class="pt-2">
+                <div v-if=borrow.lendr.lent class="pl-4 pt-2">
                   You have marked this as recieved.
                 </div>
-                <div v-else justify-center>
+                <div v-else class="pl-3">
                   <v-btn color="teal" @click="validateLend(borrow)">Have you recieved this item?</v-btn>
                 </div>
-                <div v-if=borrow.borrower.returned justify-center>
-                  This has been returned
+              </v-layout>
+              <v-layout row class="pt-2 pl-4">
+                <div v-if=borrow.lendr.returned>
+                  This item has been returned.
                 </div>
-                <div v-else class="red--text" justify-center>
-                  This item has not been returned
+                <div v-else class="red--text">
+                  Your lendr has not marked this as returned.
                 </div>
-              </v-card>
-            </v-container>
+              </v-layout>
+            </v-card>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-flex>
